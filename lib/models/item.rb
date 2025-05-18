269638c4236f7a0f9b3c7cf296c539328b5a6173
thiagopnobre <thiago_pnobre@hotmail.com>
2025-05-18 20:00:40 -0300

    refactor: add tax calc to the item

--- lib/models/item.rb
+++ lib/models/item.rb
@@ -5,13 +5,25 @@ module Lib
     class Item
       attr_reader :quantity, :product, :amount, :tax, :category, :imported
 
+      EXEMPT_CATEGORIES = %w[books food medical].freeze
+      BASIC_SALES_TAX = 0.1
+      IMPORT_DUTY = 0.05
+
       def initialize(quantity:, product:, amount:, category:, imported:)
         @quantity = quantity
         @product = product
         @amount = amount
-        @tax = 0.0
         @category = category
         @imported = imported
+        calculate_tax
+      end
+
+      private
+
+      def calculate_tax
+        @tax = 0.0
+        @tax += BASIC_SALES_TAX unless EXEMPT_CATEGORIES.include?(@category)
+        @tax += IMPORT_DUTY if imported
       end
     end
   end
--- spec/lib/models/item_spec.rb
+++ spec/lib/models/item_spec.rb
@@ -4,7 +4,7 @@ require './lib/models/item'
 
 RSpec.describe Lib::Models::Item do
   describe '#initialize' do
-    it 'initializes an item' do
+    it 'initializes an item with no tax if it is exempt and not imported' do
       item = described_class.new(
         quantity: 2,
         product: 'book',
@@ -18,7 +18,59 @@ RSpec.describe Lib::Models::Item do
       expect(item.amount).to eq 12.49
       expect(item.category).to eq 'food'
       expect(item.imported).to be false
-      expect(item.tax).to eq(0.0)
+      expect(item.tax).to eq 0.0
+    end
+
+    it 'initializes an item with import duty tax if it is exempt and imported' do
+      item = described_class.new(
+        quantity: 2,
+        product: 'book',
+        amount: 12.49,
+        category: 'food',
+        imported: true
+      )
+
+      expect(item.quantity).to eq 2
+      expect(item.product).to eq 'book'
+      expect(item.amount).to eq 12.49
+      expect(item.category).to eq 'food'
+      expect(item.imported).to be true
+      expect(item.tax).to eq described_class::IMPORT_DUTY
+    end
+
+    it 'initializes an item with basic sales tax if it is not exempt and not imported' do
+      item = described_class.new(
+        quantity: 2,
+        product: 'book',
+        amount: 12.49,
+        category: 'other',
+        imported: false
+      )
+
+      expect(item.quantity).to eq 2
+      expect(item.product).to eq 'book'
+      expect(item.amount).to eq 12.49
+      expect(item.category).to eq 'other'
+      expect(item.imported).to be false
+      expect(item.tax).to eq described_class::BASIC_SALES_TAX
+    end
+
+    it 'initializes an item with basic sales plus import duty taxes if it is not exempt and ' \
+       'it is imported' do
+      item = described_class.new(
+        quantity: 2,
+        product: 'book',
+        amount: 12.49,
+        category: 'other',
+        imported: true
+      )
+
+      expect(item.quantity).to eq 2
+      expect(item.product).to eq 'book'
+      expect(item.amount).to eq 12.49
+      expect(item.category).to eq 'other'
+      expect(item.imported).to be true
+      expect(item.tax).to eq described_class::BASIC_SALES_TAX + described_class::IMPORT_DUTY
     end
   end
 end
